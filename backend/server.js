0bab8828ac63f22aac88ec16f7dcd953bf47c5db
juliasteed <jsteed@vols.utk.edu>
2023-12-02 15:25:46 -0500

    begin adding user model

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const sendRoutes = require('./routes/sends')
+const userRoutes = require('./routes/user')
 
 const app = express()
 
@@ -15,6 +16,7 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/api/sends', sendRoutes)
+app.use('/api/user', userRoutes)
 
 // connect to database
 mongoose.connect(process.env.MONGO_URI)
