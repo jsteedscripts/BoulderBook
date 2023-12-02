ab5a44f5a229f345e7447bdc430befd2c2110a6a
juliasteed <jsteed@vols.utk.edu>
2023-12-01 21:28:28 -0500

    add mongo connection

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
 const sendRoutes = require('./routes/sends')
 
 const app = express()
 
+// middleware
 app.use(express.json())
 
 app.use((req, res, next) => {
@@ -11,8 +13,18 @@ app.use((req, res, next) => {
     next()
 })
 
+// routes
 app.use('/api/sends', sendRoutes)
 
-app.listen(process.env.PORT, () => {
-    console.log('Listening on port', process.env.PORT)
-})
+// connect to database
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connected to database')
+        // listen to port
+        app.listen(process.env.PORT, () => {
+            console.log('Listening on port', process.env.PORT)
+        })
+    })
+    .catch((err) => {
+        console.log(err)
+    })
